e0b302185e6804d3abee44dbe7659cd78171cc6a
coder-zyp <coder_zyp@163.com>
2018-05-10 18:28:26 +0800

    seniu

--- seniu11.py
+++ seniu11.py
@@ -5,49 +5,60 @@ from lxml import html
 import re
 import requests
 
-class manager():
-    url = ""
-    def __init__(self, url):
-        self.url = url
 
-    @classmethod
-    def Download(cls):
-        print(cls.url, 'asdfasdf')
+def get_html(link):
+    print(link)
+    response = requests.Session().get(link)
+    response.encoding = 'gbk'
+    return response.text
+
+
+class manager():
+
+    def __init__(self, host, url):
+        self.url = host + url
+        self.host = host
+
+
 
     def getLink(self):
-        response = requests.Session().get(self.url)
-        response.encoding = 'gbk'
 
-        sel = html.fromstring(response.text)
-        # print(response.text)
-        # 图片总数
-        texts = sel.xpath('//div[@class="text-list"]/div[@class="wrap"]/dl/dd/a/h2/text()')
+        sel = html.fromstring(get_html(self.url))
+
+        # texts = sel.xpath('//div[@class="text-list"]/div[@class="wrap"]/dl/dd/a/h2/text()')
         links = sel.xpath('//div[@class="text-list"]/div[@class="wrap"]/dl/dd/a/@href')
-        return texts, links
+        return links
 
-    def beginDownload(self, text, link):
-        print(link, threading.active_count())
-        sleep(1)
+    def beginDownload(self, link):
+        # print(self.host + link)
+        htmlstr = get_html(self.host + link)
+        sel = html.fromstring(htmlstr)
+        links = sel.xpath('//div[@class="picture"]/div[@class="wrap"]/div/img/@src')
 
 
+        title = sel.xpath('//div[@class="picture"]/div[@class="wrap"]/h1/text()')
+        print(title, links)
         # for a in alist:
         #     print(a.xpath(''))
         #     print(a.xpath('@href'))
 
-threads = []
-m = manager("http://seniu11.com/tupian/wangyouzipai/")
+m = manager("http://seniu11.com", "/tupian/wangyouzipai/")
 text_link_arr = m.getLink()
 
 
-for i in range(0, len(text_link_arr[0])):
+for i in range(0, len(text_link_arr)):
 
-    t = threading.Thread(target=m.beginDownload, args=(text_link_arr[0][i], text_link_arr[1][i]))
+    # t = threading.Thread(target=m.beginDownload, args=(text_link_arr[0][i], text_link_arr[1][i]))
+    #
+    # t.setDaemon(True)
+    #
+    # t.start()
 
-    t.setDaemon(True)
+    m.beginDownload(text_link_arr[i])
 
-    t.start()
-    while True:
-        # 判断正在运行的线程数量,如果小于5则退出while循环,
-        # 进入for循环启动新的进程.否则就一直在while循环进入死循环
-        if (threading.active_count()< 100):
-            break
+    break
+    # while True:
+    #     # 判断正在运行的线程数量,如果小于5则退出while循环,
+    #     # 进入for循环启动新的进程.否则就一直在while循环进入死循环
+    #     if (threading.active_count()< 100):
+    #         break
